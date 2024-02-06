52212f716bf484c11d65b4aa21036b6ddfccccea
visnowden <viniccamarg13@gmail.com>
2024-02-06 16:14:34 -0300

    Update verify.php

--- sum/verify.php
+++ sum/verify.php
@@ -1,6 +1,6 @@
 <?php
 $first_value = $_POST["first_value"];
 $second_value = $_POST["second_value"];
-$result = $first_value / $second_value;
+$result = $first_value + $second_value;
 echo $result
 ?>
