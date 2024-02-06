7d3d5617fe6d5306076598316f2dc7280d8880a5
visnowden <viniccamarg13@gmail.com>
2024-02-06 10:33:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>login with post</title>
 </head>
+
 <body>
-    
+    <form action="verify.php" method="POST">
+        <input type="text" name="login">
+        <input type="password" name="password">
+        <input type="submit" value="Confirm">
+    </form>
 </body>
+
 </html>
