53eae715063250c06a455c639e1f2e2ca2310a46
visnowden <viniccamarg13@gmail.com>
2024-02-06 11:15:19 -0300

    Create index.php

---
+++ sum/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sum</title>
+</head>
+
+<body>
+    <form action="verify.php" method="POST">
+        <input type="text" name="first_number">
+        <input type="text" name="second_number">
+        <input type="submit" value="Confirm">
+    </form>
+</body>
+
+</html>
