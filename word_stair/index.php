318801baf215d8e05d40860542b5cebc12beb379
visnowden <viniccamarg13@gmail.com>
2024-02-10 03:28:26 -0300

    Create index.php

---
+++ word_stair/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1\.0">
+<link rel="stylesheet" href="../style.css">
+    <title> Regressief </title>
+</head>
+<body>
+    
+<form action="verify.php" method="POST">
+        <input type="text" name="value">
+        <input type="submit" value="Confirm">
+    </form>
+</body>
+</html>
