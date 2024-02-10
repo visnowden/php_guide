36587cd39458d1c09014e5fe372bc0f1b6a706aa
visnowden <viniccamarg13@gmail.com>
2024-02-09 22:58:32 -0300

    Create index.php

---
+++ regressive/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1\.0">
+<link rel="stylesheet" href="../style.css">
+    <title> Regressief </title>
+</head>
+<body>
+    
+<form action="verify.php" method="POST">
+        <input type="number" name="first_value">
+        <input type="number" name="second_value">
+        <input type="submit" value="Confirm">
+    </form>
+</body>
+</html>
