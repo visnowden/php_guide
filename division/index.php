d34ea9af8bdbf2e81fb951b79b3a32c3e1d1c630
visnowden <viniccamarg13@gmail.com>
2024-02-09 22:58:03 -0300

    Update index.php

--- division/index.php
+++ division/index.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1\.0">
+    <link rel="stylesheet" href="../style.css">
     <title>division</title>
 </head>
 
@@ -13,6 +14,15 @@
         <input type="number" name="second_value">
         <input type="submit" value="Confirm">
     </form>
+    <p class="white_style">
+    <?php
+    if (isset($_POST["first_value"])){
+        $first_value = $_POST["first_value"];
+        $second_value = $_POST["second_value"];
+        $result = $first_value / $second_value;
+        echo $result;}
+    ?>
+    </p>
 </body>
 
 </html>
