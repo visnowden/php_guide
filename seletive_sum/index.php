656bc87d5647072ea1d1490b18866ab1f0dc3c0f
visnowden <viniccamarg13@gmail.com>
2024-02-10 03:28:07 -0300

    Create index.php

---
+++ seletive_sum/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1\.0">
+<link rel="stylesheet" href="../style.css">
+    <title> Regressief </title>
+</head>
+<body>
+    
+<form action="verify.php" method="POST">
+        <input type="number" name="first_value">
+        <input type="number" name="second_value">
+        <input type="number" name="third_value">
+        <input type="number" name="fourth_value">
+        <input type="number" name="fifth_value">
+        <input type="number" name="sixth_value">
+        <input type="number" name="seventh_value">
+        <input type="number" name="eighth_value">
+        <input type="number" name="ninth_value">
+        <input type="number" name="tenth_value">
+        <input type="number" name="eleventh_value">
+        <input type="number" name="twelfth_value">
+        <input type="number" name="thirteenth_value">
+        <input type="number" name="fourteenth_value">
+        <input type="number" name="fifteenth_value">
+        <input type="number" name="sixteenth_value">
+        <input type="number" name="seventeenth_value">
+        <input type="number" name="eighteenth_value">
+        <input type="number" name="nineteenth_value">
+        <input type="number" name="twentieth_value">
+        <input type="submit" value="Confirm">
+    </form>
+</body>
+</html>
