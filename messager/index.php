1b13c531177c33c3b8e2b7c93b096d9a6f7961b2
Code_H29/ <viniccamarg13@gmail.com>
2024-03-26 12:16:04 -0300

    Create index.php

---
+++ messager/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Message </title>
+</head>
+    <body>
+        <center>
+            <div style="top: 5em; position: relative;">
+                <form action="verify.php" method="post">
+                    <input type="text" name="user">
+                    <br>
+                    <input type="password" name="pass">
+                    <br>
+                    <input style="margin-top: 0.5em;" type="submit" value="Login">
+                </form>
+                <a href="./sign_up">Sign up</a>
+            </div>
+        </center>
+    </body>
+</html>
