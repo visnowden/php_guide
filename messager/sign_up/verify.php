e17c41de0cde024952281221102ab3d43c0561c1
Code_H29/ <viniccamarg13@gmail.com>
2024-05-03 11:00:56 -0300

    Update verify.php

--- messager/sign_up/verify.php
+++ messager/sign_up/verify.php
@@ -1,31 +1,30 @@
 <?php
-$hostname = "127.0.0.1";
-$user = "root";
-$password = "";
-$database = "web";
-$port = 3305;
+    $hostname = "127.0.0.1";
+    $user = "root";
+    $password = "";
+    $database = "web";
 
-$connection = new mysqli($hostname, $user, $password, $port);
+    $connection = new mysqli($hostname, $user, $password, $database);
 
-if ($connection->connect_errno) {
-    exit();
-} else {
-    $user_name = $_POST['user'];
-    if ($_POST['pass_'] != $_POST['pass']) {
-        header('Location: .', true, 301);
-        $_POST['message'] = 'Passwords don\'t coincide!';
-        echo $_POST['message'];
+    if ($connection->connect_errno) {
+        exit();
     } else {
-        $password = $_POST['pass'];
-        date_default_timezone_set('America/Sao_Paulo');
-        $sql = "INSERT INTO `web`.`user`
-            (`user_name`, `password`, `registration_date`, `active`)
-            VALUES
-            ('" . $user_name . "', '" . $password . "', '" . date("Y-m-d H:i:s") . "', true);";
+        $user_name = $_POST['user'];
+        if ($_POST['pass_'] != $_POST['pass']) {
+            header('Location: .', true, 301);
+            $_POST['message'] = 'Passwords don\'t coincide!';
+            echo $_POST['message'];
+        } else {
+            $password = $_POST['pass'];
+            date_default_timezone_set('America/Sao_Paulo');
+            $sql = "INSERT INTO `web`.`user`
+                (`user_name`, `password`, `registration_date`, `active`)
+                VALUES
+                ('" . $user_name . "', '" . $password . "', '" . date("Y-m-d H:i:s") . "', true);";
 
-        $resultado = $connection->query($sql);
+            $resultado = $connection->query($sql);
 
-        $connection->close();
-        header('Location: ..', true, 301);
+            $connection->close();
+            header('Location: ..', true, 301);
+        }
     }
-}
